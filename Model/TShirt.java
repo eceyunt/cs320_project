0a6d95195bbd18975ec82888b30f89966e9c19a7
erenatsiz <36725949+erenatsiz@users.noreply.github.com>
2018-12-23 21:19:21 +0300

    Create Account, Product and some related classes. (Jean, Shoes etc.)

---
+++ Model/TShirt.java
@@ -0,0 +1,25 @@
+package Model;
+
+
+
+public class TShirt extends Product {
+    private static final int UNIT_TYPE = 2;
+
+    public TShirt(String name, double price, String productDesc) {
+        super(name, price, UNIT_TYPE);
+        super.setProductDesc(productDesc);
+    }
+
+    public TShirt(String name, double price) {
+        super(name, price, UNIT_TYPE);
+        super.setProductDesc("Description not mentioned.");
+    }
+
+    public String toString() {
+        return getProductDesc() + " " +
+                getProductName() + ", " +
+                getProductPrice() + " TL " +
+                getUnitType() + ".";
+    }
+}
+
